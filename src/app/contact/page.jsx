cc436e1a3a500508b8916703bb819a603922e908
Jakub <jakubglinka9@gmail.com>
2023-09-02 16:02:36 +0200

    Add contact and portfolio page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="" fill className={styles.img} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input
+            type="email"
+            placeholder="email"
+            className={styles.input}
+          />
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="message"
+            className={styles.input}
+          ></textarea>
+          <Button url="#">Send</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import { items } from "./data";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
-const Category = () => {
+const getData = (category) => {
+  const data = items[category];
+
+  if (data) {
+    return data;
+  }
+
+  notFound();
+};
+
+const Category = ({ params }) => {
+  const data = getData(params.category);
+  console.log(data);
+
+  const capitalisedCatTitle =
+    params.category.charAt(0).toUpperCase() + params.category.slice(1);
+
   return (
-    <div className={styles.container}>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{capitalisedCatTitle}</h2>
+      <div className={styles.itemsContainer}>
+        {data.map((item) => (
+          <div className={styles.item} key={item.id}>
+            <div className={styles.text}>
+              <h1>{item.title}</h1>
+              <p>{item.desc}</p>
+              <Button url="#">See more</Button>
+            </div>
+            <div className={styles.imageContainer}>
+              <Image src={item.image} alt="" fill className={styles.img} sizes="100vw"/>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
