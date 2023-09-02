183929b1088c5d4ccacdd3ffcabd63cca6821b0f
Jakub <jakubglinka9@gmail.com>
2023-09-02 16:06:28 +0200

    fix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Button from "@/components/Button/Button";
 
 export const metadata = {
-  title: "about as",
+  title: "about",
   description: "About NextX",
 };
 
