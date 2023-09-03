1ea4fae4112261eb1a1b7f6a74f9851710d48dff
Jakub <jakubglinka9@gmail.com>
2023-09-03 08:33:37 +0200

    Add server side fetching

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -2,12 +2,29 @@ import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 
-const BlogId = () => {
+export const metadata = {
+  title: "blog",
+  description: "NextX blog page",
+};
+
+async function getData(postId) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const BlogId = async ({params}) => {
+  const data = await getData(params.id)
+
   return (
     <div className={styles.container}>
       <div className={styles.leftPanel}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>Desc</p>
+        <h1 className={styles.title}>{data.title}</h1>
+        <p className={styles.desc}>{data.body}</p>
         <div className={styles.logo}>
           <Image src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" width={80} height={80} />
           <div>Author</div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,36 +8,38 @@ export const metadata = {
   description: "NextX blog page",
 };
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+
   return (
     <div className={styles.container}>
-      <Link className={styles.item} href="/blog/blogTitle">
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-            fill
-            sizes="400px"
-            className={styles.img}
-          />
-        </div>
-        <div className={styles.text}>
-          <h1>Blog</h1>
-          <p>Desc</p>
-        </div>
-      </Link>
-      <Link className={styles.item} href="/blog/blogTitle">
-        <Image
-          src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600"
-          alt=""
-          width={400}
-          height={250}
-        />
-        <div className={styles.text}>
-          <h1>Blog</h1>
-          <p>Desc</p>
-        </div>
-      </Link>
+      {data.map((post) => (
+        <Link className={styles.item} href={`blog/${post.id}`} key={post.id}>
+          <div className={styles.imgContainer}>
+            <Image
+              src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+              fill
+              sizes="400px"
+              className={styles.img}
+            />
+          </div>
+          <div className={styles.text}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
