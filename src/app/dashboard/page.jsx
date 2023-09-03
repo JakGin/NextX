627686bc9f63425537b1f103d0ee1c7f3ff9e68d
Jakub <jakubglinka9@gmail.com>
2023-09-03 11:52:22 +0200

    fetch data on the client side

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import styles from "./page.module.css"
+"use client"
+
+import React from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
 
 export const metadata = {
   title: "dashboard",
   description: "Admin dashboard",
 };
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 const Dashboard = () => {
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <div className={styles.container}>
       <h1>Dashboard</h1>
+      <p>{data[0]?.title}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
