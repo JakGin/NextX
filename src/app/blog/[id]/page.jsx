7b6d8d44faf03f1756b9d5dbc478368ee636810b
Jakub <jakubglinka9@gmail.com>
2023-09-03 06:57:14 +0200

    Add blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const BlogId = () => {
   return (
-    <div className={styles.container}>BlogId</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.leftPanel}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>Desc</p>
+        <div className={styles.logo}>
+          <Image src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" width={80} height={80} />
+          <div>Author</div>
+        </div>
+        <p className={styles.content}>Content</p>
+      </div>
+      <div className={styles.rightPanel}>
+      <Image src="https://images.pexels.com/photos/3913025/pexels-photo-3913025.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" fill sizes="600px" />
+      </div>
+    </div>
+  );
+};
 
-export default BlogId
+export default BlogId;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import styles from "./page.module.css"
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: "blog",
@@ -9,9 +11,35 @@ export const metadata = {
 const Blog = () => {
   return (
     <div className={styles.container}>
-      <h1>Blog</h1>
+      <Link className={styles.item} href="/blog/blogTitle">
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+            fill
+            sizes="400px"
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.text}>
+          <h1>Blog</h1>
+          <p>Desc</p>
+        </div>
+      </Link>
+      <Link className={styles.item} href="/blog/blogTitle">
+        <Image
+          src="https://images.pexels.com/photos/8438944/pexels-photo-8438944.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+          width={400}
+          height={250}
+        />
+        <div className={styles.text}>
+          <h1>Blog</h1>
+          <p>Desc</p>
+        </div>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
